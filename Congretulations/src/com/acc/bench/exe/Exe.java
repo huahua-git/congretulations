f63d82309d4eaab18402a03debbf48eb05b2d60b
huahua-git <jeff_12345678@163.com>
2018-02-27 11:40:28 +0800

    dddd

--- Congretulations/src/com/acc/bench/exe/Exe.java
+++ Congretulations/src/com/acc/bench/exe/Exe.java
@@ -3,7 +3,7 @@ package com.acc.bench.exe;
 public class Exe {
 
 	public static void main(String[] args) {
-		System.out.println("Hello OnBench");
+		System.out.println("Hello OnBench...");
 	}
 
 }
